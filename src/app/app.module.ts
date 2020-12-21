dba5ba5d259c89856eb4cf30939c5c2576164e86
jackkweyunga <75433841+jackkweyunga@users.noreply.github.com>
2020-12-22 00:15:37 +0300

    added the footer
    It has some social links. description and topics overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,15 @@ import {AppRoutingModule} from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import { FooterComponent } from './footer/footer.component';
+import { TrendingComponent } from './trending/trending.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    FooterComponent,
+    TrendingComponent
   ],
   imports: [
     BrowserModule,
