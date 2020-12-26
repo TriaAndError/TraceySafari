7374c02cc2a033084de525f1742f015f4421d137
Jek <jackkweyunga@gmail.com>
2020-12-26 19:14:08 +0300

    added some in page animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TraceySafari';
+  	title = 'TraceySafari';
+
+	ngOnInit(): void {
+  		AOS.init();
+  	}
+  
 }
