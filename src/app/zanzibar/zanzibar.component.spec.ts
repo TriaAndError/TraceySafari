895fb3e58470f93f5b991fe03a613d93a528c300
horaceTorvalds <fredy.masika@gmail.com>
2021-01-30 19:47:52 +0300

    added team comp.

---
+++ src/app/zanzibar/zanzibar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ZanzibarComponent } from './zanzibar.component';
+
+describe('ZanzibarComponent', () => {
+  let component: ZanzibarComponent;
+  let fixture: ComponentFixture<ZanzibarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ZanzibarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ZanzibarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
